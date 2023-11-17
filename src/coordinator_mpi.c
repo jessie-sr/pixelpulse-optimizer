dbf02cb6080cf94ea6fe2f21bc797ef75cf1b2a7
Rong Sun <jessie_sr@berkeley.edu>
2023-11-16 16:44:22 -0800

    Added MPI Optimization

--- src/coordinator_mpi.c
+++ src/coordinator_mpi.c
@@ -15,22 +15,60 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  // Read and parse task list file
+  // TODO: implement Open MPI coordinator
+
+  MPI_Init(&argc, &argv);
+
+  int procID, totalProcs;
+
+  MPI_Comm_size(MPI_COMM_WORLD, &totalProcs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &procID);
+
   int num_tasks;
-  task_t **tasks;
-  if (read_tasks(argv[1], &num_tasks, &tasks)) {
-    printf("Error reading task list from %s\n", argv[1]);
-    return -1;
+  task_t **output_tasks;
+
+  if (procID == 0) {
+
+      int nextTask = 0;
+      MPI_Status status;
+      if (read_tasks(argv[1], &num_tasks, &output_tasks)) {
+          return -1;
+      }
+
+      int32_t message;
+
+      while (nextTask < num_tasks) {
+          MPI_Recv(&message, 1, MPI_INT32_T, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+          int sourceProc = status.MPI_SOURCE;
+          message = nextTask;
+          MPI_Send(&message, 1, MPI_INT32_T, sourceProc, 0, MPI_COMM_WORLD);
+          nextTask++;
   }
 
-  // Execute tasks
-  for (int i = 0; i < num_tasks; i++) {
-    if (execute_task(tasks[i])) {
-      printf("Task %d failed\n", i);
-      return -1;
-    }
-    free(tasks[i]->path);
-    free(tasks[i]);
+      for (int i = 0; i < totalProcs - 1; i++) {
+          MPI_Recv(&message, 1, MPI_INT32_T, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+          int sourceProc = status.MPI_SOURCE;
+          message = TERMINATE;
+          MPI_Send(&message, 1, MPI_INT32_T, sourceProc, 0, MPI_COMM_WORLD);
+      }
+
+  } else {
+
+      int32_t message;
+
+      if (read_tasks(argv[1], &num_tasks, &output_tasks)) {
+        return -1;
+      }
+
+      while (true) {
+          message = READY;
+          MPI_Send(&message, 1, MPI_INT32_T, 0, 0, MPI_COMM_WORLD);
+          MPI_Recv(&message, 1, MPI_INT32_T, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          if (message == TERMINATE) break;
+          execute_task(output_tasks[message]);
+      }
   }
-  free(tasks);
+
+  MPI_Finalize();
+  return 0;
 }
